04936a3ba7b6f714dc17efb80876db20823c7ae7
Esmail <elmoussel12@gmail.com>
2020-09-03 01:51:08 +0200

    create single article page

--- articles/views.py
+++ articles/views.py
@@ -5,3 +5,8 @@ from .models import Article
 def articles_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/articles.html', {'articles': articles})
+
+
+def article_details(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_details.html', {'article': article})
