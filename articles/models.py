cb9323596ec3fa33b666ae29b94872e5ca8310a8
Esmail <elmoussel12@gmail.com>
2020-09-05 02:20:34 +0200

    final touch

--- articles/models.py
+++ articles/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     body = models.TextField()
     date = models.DateField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
